bd168391841f8b43dec91da2009ea28aab934c98
xodnd0507 <24457545yong@gmail.com>
2024-05-21 14:20:47 +0900

    docs : Delete text

--- processD.php
+++ processD.php
@@ -8,21 +8,6 @@
 <body>
 <?php
         $conn = mysqli_connect( 'localhost', 'root', '', 'company');
-
-        // settype($_POST['id'], 'integer');
-        // $filtered = array(
-        //     'id'=>mysqli_real_escape_string($conn, $_POST['id'])
-        // );
-        // $sql = "DELETE FROM company WHERE id = {$filtered['id']}";
-        // $result = mysqli_query($conn, $sql);
-        // if($result === false){
-        //     echo '저장하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요';
-        //     error_log(mysqli_error($conn));
-        // } else {
-        //     echo '삭제했습니다. <a href="index.php">돌아가기</a>';
-        //     error_log(mysqli_error($conn));
-        // };
-
         $id = $_POST['id'];
 
         $sql = "DELETE FROM company WHERE id = {$id}";
